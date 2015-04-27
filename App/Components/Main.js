f47a5931f086d0f9d47d2fa3ec3ad94db905c90a
Robert Ing <rmi22186@gmail.com>
2015-04-27 01:29:45 -0400

    (feat) Add status indicator, and error messaging on username input page

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -101,6 +101,10 @@ class Main extends React.Component{
   }
     //reroute to the next page, passing that github info in
   render() {
+    var showErr = (
+      this.state.error ? <Text> {this.state.error} </Text> : <View></View>
+    );
+
     return(
       <View style={styles.mainContainer}>
       <Text style={styles.title}> Search for a Github User </Text>
@@ -114,6 +118,12 @@ class Main extends React.Component{
           underlayColor="white">
           <Text style={styles.buttonText}> SEARCH </Text>
         </TouchableHighlight>
+        <ActivityIndicatorIOS
+          animating={this.state.isLoading}
+          color='#111'
+          size="large">
+        </ActivityIndicatorIOS>
+        {showErr}
       </View>
       )
   }
