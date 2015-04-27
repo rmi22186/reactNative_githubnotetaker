11ca742bfe2ca6a139d228e1cab7227c8a61d6ce
Robert Ing <rmi22186@gmail.com>
2015-04-27 01:44:06 -0400

    (feat) add repo, notes, and profile buttons to dashboard

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -5,7 +5,9 @@ var React = require('react-native');
 var {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Image,
+  TouchableHighlight
 } = React;
 
 var styles = StyleSheet.create({
@@ -25,11 +27,56 @@ var styles = StyleSheet.create({
 
 
 class Dashboard extends React.Component{
+  makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    };
+
+    if (btn === 0) {
+      obj.backgroundColor = '#888888';
+    } else if (btn === 1) {
+      obj.backgroundColor = '#e77aae';
+    } else {
+      obj.backgroundColor = '#758bf4';
+    }
+
+    return obj;
+  } 
+
+  goToProfile() {
+    console.log('profile');
+  }
+  goToRepos() {
+    console.log('repo');
+  }
+  goToNotes() {
+    console.log('notes');
+  }
   render(){
     return (
       <View style={styles.container}>
-        <Text> This is the dashboard </Text>
-        <Text> {this.props.userInfo} </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor="#88d4f5">
+          <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+          underlayColor="#88d4f5">
+          <Text style={styles.buttonText}> View Repos </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+          underlayColor="#88d4f5">
+          <Text style={styles.buttonText}> View Notes </Text>
+        </TouchableHighlight>
       </View>
     )
   }
